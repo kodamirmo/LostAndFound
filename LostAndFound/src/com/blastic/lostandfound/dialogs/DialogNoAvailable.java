1fa083f734edf95713ba8541f0ab7a18d0745ba9
Irving Emmanuel Gonzalez <irvingemmanuelgg@gmail.com>
2014-04-12 01:58:41 -0500

    Resolve dialogs issue

--- LostAndFound/src/com/blastic/lostandfound/dialogs/DialogNoAvailable.java
+++ LostAndFound/src/com/blastic/lostandfound/dialogs/DialogNoAvailable.java
@@ -1,51 +1,39 @@
 package com.blastic.lostandfound.dialogs;
 
-import com.blastic.lostandfound.R;
-import com.blastic.lostandfound.SimilarReportsScreen;
-
+import com.blastic.lostandfound.LoginScreen;
+import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.LinearLayout;
-import android.widget.Toast;
 
 public class DialogNoAvailable extends DialogFragment{
 
 	@Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
 
-		final Dialog dialog = new Dialog(getActivity());
-		dialog.setTitle("Opciones");
-		dialog.setContentView(R.layout.dialog_view);
-
-		LinearLayout optionSimilar = (LinearLayout) dialog.findViewById(R.id.optsDialogSimilary);
-		LinearLayout optionIndequate = (LinearLayout) dialog.findViewById(R.id.optsDialogInadequate);
-		
-		optionSimilar.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				Intent reports = new Intent(getActivity(),SimilarReportsScreen.class);
-				startActivity(reports);
-				dialog.dismiss();
-			}
-		});
-
-		optionIndequate.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-
-				Toast.makeText(getActivity(),
-								"¡Gracias por tu aporte! El reporte ya fue enviado a revisión",Toast.LENGTH_SHORT).show();
-				dialog.dismiss();
-			}
-		});
-		
-		return dialog;	
+        return new AlertDialog.Builder(getActivity())
+                .setMessage("Como invitado, no puedes usar esta opcion. ¿Iniciar sesion?")
+                .setTitle("Invitado")
+                .setPositiveButton("Iniciar sesion",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int whichButton) {     
+                            dialog.cancel();
+                            Intent goToLogin=new Intent(getActivity(), LoginScreen.class);
+                            goToLogin.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                            getActivity().startActivity(goToLogin);
+                        }
+                    }
+                )
+                .setNegativeButton("Cancelar",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int whichButton) {
+                           dialog.cancel();
+                        }
+                    }
+                )
+                .create();
     }
 	
 }
